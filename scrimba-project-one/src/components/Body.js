35bd3840b44fe0b89e594715d5688c2663bc12b9
J-Skarbek <john@snarkmediaagency.com>
2023-02-12 12:13:37 -0600

    Work on static page layout

---
+++ scrimba-project-one/src/components/Body.js
@@ -0,0 +1,18 @@
+import '../body.css';
+
+function Body() {
+  return (
+    <div className="body">
+      <h1>Fun Facts About React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K starts on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Body;
---
+++ scrimba-project-one/src/components/NavBar.js
@@ -0,0 +1,14 @@
+
+// import ReactLogo from '../react-logo.png';
+import Logo from '../logo.svg';
+import '../NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="nav-bar">
+      <img src={Logo} width = "100px" alt=""/>
+    </div>
+  );
+}
+
+export default NavBar;
