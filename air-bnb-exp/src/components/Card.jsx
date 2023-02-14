ebee0f2088d41eec80bd89e8cf597079f8ff1df3
J-Skarbek <john@snarkmediaagency.com>
2023-02-14 14:07:12 -0600

    Fill out props in each card component

--- air-bnb-exp/src/components/Card.jsx
+++ air-bnb-exp/src/components/Card.jsx
@@ -3,7 +3,7 @@ import star from '../assets/Star.svg';
 import dot from '../assets/dot.svg';
 import '../styles/card.css';
 
-function Card() {
+function Card(props) {
   return (
     <div className="card">
       <div className="status">
@@ -13,11 +13,11 @@ function Card() {
         <img src={cardImg} alt="" />
       </div>
       <div className="details">
-        <img src={star} alt="" />
+        <img src={star} alt="" className="star"/>
         <p>5.0</p>
-        <p>(6)</p>
-        <img src={dot} alt="" />
-        <p>USA</p>      
+        <p className="gray-text">(6)</p>
+        <img src={dot} alt="" className="dot" />
+        <p className="gray-text">USA</p>      
       </div>
       <div className="title">
         <p>Life Lessons with Katie Zaferes</p>
--- air-bnb-exp/src/components/CardsDisplay.jsx
+++ air-bnb-exp/src/components/CardsDisplay.jsx
@@ -4,7 +4,33 @@ import '../styles/cardDisplay.css';
 function CardsDisplay() {
   return (
     <div className="cards-display">
-       <Card />
+       <Card
+        title="Life lessons with Katie Zaferes"
+        img="../assets/katie-zaferes.jpg"
+        status="SOLD OUT"
+        rating="5.0"
+        numberOfReviews="6"
+        country="USA"
+        pricePerPerson="136"
+      />
+       <Card 
+        title="Learn Wedding Photography"
+        img="../assets/katie-zaferes.jpg"
+        status="ONLINE"
+        rating="5.0"
+        numberOfReviews="30"
+        country="USA"
+        pricePerPerson="125"
+       />
+       <Card 
+        title="Group Mountain Biking"
+        img="../assets/katie-zaferes.jpg"
+        status="Online"
+        rating="4.8"
+        numberOfReviews="2"
+        country="USA"
+        pricePerPerson="50"
+       />
     </div>
   );
 }
