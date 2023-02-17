c80307cc2394a64a7e8fc5462843cd3c3831d040
J-Skarbek <john@snarkmediaagency.com>
2023-02-17 15:56:27 -0600

    Create function to get random meme url

--- meme-generator/src/components/Input.jsx
+++ meme-generator/src/components/Input.jsx
@@ -1,15 +1,21 @@
 import '../styles/Input.css';
+import memesData from '../memesData';
 
 function Input() {
-  function handleClick() {
-    console.log('the button was clicked.')
+  const memesArray = memesData.data.memes;
+  let url;
+  function getMemeImg() {
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    console.log(randomNumber);
+    url = memesArray[randomNumber].url;
+    console.log(url);
   }
   return (
     <div className="user-input flex items-center justify-center px-8 my-8">
       <form className="flex items-center justify-center flex-wrap gap-x-4 max-w-3xl">
         <input type="text" placeholder="Enter Top Line" className="min-w-fortyEight rounded-3xl"></input>
         <input type="text" placeholder="Enter Bottom Line" className="min-w-fortyEight rounded-3xl"></input>
-        <button type="button" onClick={handleClick} className="text-white font-bold bg-gradient-to-r from-darkPurple to-lightPurple grow-2 my-4 rounded-3xl">Get New Meme Image</button>
+        <button type="button" onClick={getMemeImg} className="text-white font-bold bg-gradient-to-r from-darkPurple to-lightPurple grow-2 my-4 rounded-3xl">Get New Meme Image</button>
       </form>
     </div>
   );
