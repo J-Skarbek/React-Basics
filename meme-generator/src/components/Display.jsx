9d64ac4e8cdc4941959d489d71054567b435db62
J-Skarbek <john@snarkmediaagency.com>
2023-02-17 16:00:41 -0600

    Move the meme img into its own component

--- meme-generator/src/components/Display.jsx
+++ meme-generator/src/components/Display.jsx
@@ -1,11 +1,12 @@
 import '../styles/Display.css';
 import dummyImg from '../assets/dummyImg.jpg';
+import Meme from './Meme';
 import memesData from '../memesData';
 
 function Display() {
   return (
     <div className="display flex flex-col justify-center items-center relative">
-      <img src={dummyImg} alt="" className="w-9/12"/>
+      <Meme />
       <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-top-line absolute top-8">Shut Up</span>
       <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-bottom-line absolute bottom-8">And Take My Money</span>
     </div>
---
+++ meme-generator/src/components/Meme.jsx
@@ -0,0 +1,10 @@
+import dummyImg from '../assets/dummyImg.jpg';
+import memesData from '../memesData';
+
+function Meme() {
+  return (
+    <img src={dummyImg} alt="" className="w-9/12"/>
+  );
+}
+
+export default Meme;
