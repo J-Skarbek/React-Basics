be07e515415999847008a8953c0fe7b0afc9670f
J-Skarbek <john@snarkmediaagency.com>
2023-02-17 15:11:34 -0600

    Add temp api return data and test event handlers

--- meme-generator/src/components/Display.jsx
+++ meme-generator/src/components/Display.jsx
@@ -2,9 +2,12 @@ import '../styles/Display.css';
 import dummyImg from '../assets/dummyImg.jpg';
 
 function Display() {
+  function imgFocus() {
+    console.log('The mouse went over the image.')
+  }
   return (
     <div className="display flex flex-col justify-center items-center relative">
-      <img src={dummyImg} alt="" className="w-9/12"/>
+      <img src={dummyImg} onMouseOver={imgFocus} alt="" className="w-9/12"/>
       <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-top-line absolute top-8">Shut Up</span>
       <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-bottom-line absolute bottom-8">And Take My Money</span>
     </div>
--- meme-generator/src/components/Input.jsx
+++ meme-generator/src/components/Input.jsx
@@ -1,12 +1,15 @@
 import '../styles/Input.css';
 
 function Input() {
+  function handleClick() {
+    console.log('the button was clicked.')
+  }
   return (
     <div className="user-input flex items-center justify-center px-8 my-8">
       <form className="flex items-center justify-center flex-wrap gap-x-4 max-w-3xl">
         <input type="text" placeholder="Enter Top Line" className="min-w-fortyEight rounded-3xl"></input>
         <input type="text" placeholder="Enter Bottom Line" className="min-w-fortyEight rounded-3xl"></input>
-        <button type="button" className="text-white font-bold bg-gradient-to-r from-darkPurple to-lightPurple grow-2 my-4 rounded-3xl">Get New Meme Image</button>
+        <button type="button" onClick={handleClick} className="text-white font-bold bg-gradient-to-r from-darkPurple to-lightPurple grow-2 my-4 rounded-3xl">Get New Meme Image</button>
       </form>
     </div>
   );
