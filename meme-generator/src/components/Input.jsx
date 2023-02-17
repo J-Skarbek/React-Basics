d8b653a6da94f49e4386fd840a13c0f4bc789298
J-Skarbek <john@snarkmediaagency.com>
2023-02-17 12:52:19 -0600

    Style the form inputs

--- meme-generator/src/components/Input.jsx
+++ meme-generator/src/components/Input.jsx
@@ -2,11 +2,11 @@ import '../styles/Input.css';
 
 function Input() {
   return (
-    <div className="user-input flex">
-      <form>
-        <input type="text" placeholder="Enter Top Line"></input>
-        <input type="text" placeholder="Enter Bottom Line"></input>
-        <button type="button">Get New Meme Image</button>
+    <div className="user-input flex items-center justify-center px-8 my-8">
+      <form className="flex items-center justify-center flex-wrap gap-x-4 max-w-3xl">
+        <input type="text" placeholder="Enter Top Line" className="min-w-fortyEight"></input>
+        <input type="text" placeholder="Enter Bottom Line" className="min-w-fortyEight"></input>
+        <button type="button" className="text-white bg-gradient-to-r from-darkPurple to-lightPurple grow-2 my-4">Get New Meme Image</button>
       </form>
     </div>
   );
--- meme-generator/src/components/Navbar.jsx
+++ meme-generator/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="navbar flex items-center justify-between bg-gradient-to-r from-darkPurple to-lightPurple px-8">
       <div className="logo-and-title flex items-center">
-      <img src={logo} alt="Meme Generator Logo" className="w-12" />
+      <img src={logo} alt="Meme Generator Logo" className="w-12 mr-4" />
       <h1 className="font-karla font-bold text-white">Meme Generator</h1>
       </div>
       <span className="font-karla text-white">React Course - Project 3</span>
