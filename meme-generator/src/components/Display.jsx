56769e574941ce76d6f5084923788d3977e34b4f
J-Skarbek <john@snarkmediaagency.com>
2023-02-18 13:57:45 -0600

    Consolidate the meme/input components into the display component and remove unneeded files

--- meme-generator/src/components/Display.jsx
+++ meme-generator/src/components/Display.jsx
@@ -1,15 +1,32 @@
 import '../styles/Display.css';
-import { Meme, getMemeImg } from './Meme';
+// import { Meme } from './Meme';
+import memesData from '../memesData';
+import React from 'react';
 
 function Display() {
+  const [memeImg, setMemeImg] = React.useState('');
+
+  function getMeme() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImg(memesArray[randomNumber].url);
+  }
+  
   return (
-    <div className="display flex flex-col justify-center items-center relative">
-      {<Meme 
-        url={`${getMemeImg()}`}
-      />}
-      <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-top-line absolute top-8">Shut Up</span>
-      <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-bottom-line absolute bottom-8">And Take My Money</span>
-    </div>
+    <main>
+      <div className="user-input flex items-center justify-center px-8 my-8">
+        <div className="flex items-center justify-center flex-wrap gap-x-4 max-w-3xl">
+          <input type="text" placeholder="Enter Top Line" className="min-w-fortyEight rounded-3xl"></input>
+          <input type="text" placeholder="Enter Bottom Line" className="min-w-fortyEight rounded-3xl"></input>
+          <button type="button" onClick={getMeme} className="text-white font-bold bg-gradient-to-r from-darkPurple to-lightPurple grow-2 my-4 rounded-3xl">Get New Meme Image</button>
+        </div>
+      </div>
+      <div className="display flex flex-col justify-center items-center relative min-h-[75%]">
+      <img src={memeImg} alt="" className="w-9/12"/>
+        <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-top-line absolute top-8">Shut Up</span>
+        <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-bottom-line absolute bottom-8">And Take My Money</span>
+      </div>
+    </main>
   );
 }
 
--- meme-generator/src/main.jsx
+++ meme-generator/src/main.jsx
@@ -2,14 +2,12 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './components/Navbar';
-import { Input } from './components/Input';
 import Display from './components/Display';
 import Footer from './components/Footer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
-    <Input />
     <Display />
     <Footer />
   </React.StrictMode>,
