b7c426ccfbe18494a66558641d0fb748102cc878
J-Skarbek <john@snarkmediaagency.com>
2023-03-01 10:46:13 -0600

    Start on adding state to word-line elements

--- meme-generator/src/components/Display.jsx
+++ meme-generator/src/components/Display.jsx
@@ -22,13 +22,30 @@ function Display() {
       randomImage: url
     }));
   }
+
+  function handleTextChange(event) {
+    const {name} = event.target;
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      topText: '',
+      bottomText: '',
+    }))
+  }
+
+  function updateText() {
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      topText: '',
+      bottomText: '',
+    }))
+  }
   
   return (
     <main className="min-h-full">
       <div className="user-input flex items-center justify-center px-8 my-8">
         <div className="flex items-center justify-center flex-wrap gap-x-4 max-w-3xl">
-          <input type="text" placeholder="Enter Top Line" className="min-w-fortyEight rounded-3xl"></input>
-          <input type="text" placeholder="Enter Bottom Line" className="min-w-fortyEight rounded-3xl"></input>
+          <input type="text" name="topText" placeholder="Enter Top Line" className="min-w-fortyEight rounded-3xl"></input>
+          <input type="text" name="bottomText" placeholder="Enter Bottom Line" className="min-w-fortyEight rounded-3xl"></input>
           <button type="button" onClick={getMeme} className="text-white font-bold bg-gradient-to-r from-darkPurple to-lightPurple grow-2 my-4 rounded-3xl">Get New Meme Image</button>
         </div>
       </div>
