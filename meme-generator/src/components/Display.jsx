cd81f246834d49125214ff6ef9d2cec20f7ac95a
J-Skarbek <john@snarkmediaagency.com>
2023-03-01 10:54:28 -0600

    State and form updates for text elements completed

--- meme-generator/src/components/Display.jsx
+++ meme-generator/src/components/Display.jsx
@@ -24,19 +24,10 @@ function Display() {
   }
 
   function handleTextChange(event) {
-    const {name} = event.target;
+    const {name, value} = event.target;
     setMeme(prevMeme => ({
       ...prevMeme,
-      topText: '',
-      bottomText: '',
-    }))
-  }
-
-  function updateText() {
-    setMeme(prevMeme => ({
-      ...prevMeme,
-      topText: '',
-      bottomText: '',
+      [name]: value
     }))
   }
   
@@ -44,15 +35,40 @@ function Display() {
     <main className="min-h-full">
       <div className="user-input flex items-center justify-center px-8 my-8">
         <div className="flex items-center justify-center flex-wrap gap-x-4 max-w-3xl">
-          <input type="text" name="topText" placeholder="Enter Top Line" className="min-w-fortyEight rounded-3xl"></input>
-          <input type="text" name="bottomText" placeholder="Enter Bottom Line" className="min-w-fortyEight rounded-3xl"></input>
-          <button type="button" onClick={getMeme} className="text-white font-bold bg-gradient-to-r from-darkPurple to-lightPurple grow-2 my-4 rounded-3xl">Get New Meme Image</button>
+          <input 
+            type="text"
+            name="topText"
+            onChange={handleTextChange}
+            value={meme.topText}
+            placeholder="Enter Top Line"
+            className="min-w-fortyEight rounded-3xl">
+          </input>
+          <input
+            type="text"
+            name="bottomText"
+            onChange={handleTextChange}
+            value={meme.bottomText}
+            placeholder="Enter Bottom Line"
+            className="min-w-fortyEight rounded-3xl">
+          </input>
+          <button
+            type="button"
+            onClick={getMeme}
+            className="text-white font-bold bg-gradient-to-r from-darkPurple to-lightPurple grow-2 my-4 rounded-3xl">
+            Get New Meme Image
+          </button>
         </div>
       </div>
       <div className="display flex flex-col justify-center items-center relative h-5/6 min-h-[500px]">
       <img src={meme.randomImage} alt="" className="w-9/12 h-memeHeight max-h-memeMaxHeight object-contain"/>
-        <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-top-line absolute top-8">Shut Up</span>
-        <span className="meme-text font-karla font-black text-white text-6xl uppercase dummy-bottom-line absolute bottom-8">And Take My Money</span>
+        <span
+          className="meme-text font-karla font-black text-white text-6xl uppercase dummy-top-line absolute top-8">
+          {meme.topText}
+        </span>
+        <span
+          className="meme-text font-karla font-black text-white text-6xl uppercase dummy-bottom-line absolute bottom-8">
+          {meme.bottomText}
+        </span>
       </div>
     </main>
   );
